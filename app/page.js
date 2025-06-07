e09773828a2bfc317f36e239506c5b7413b3cd00
Abhishek-Narkhede <abhisheknarkhede@sdlccorp.com>
2025-06-08 01:28:20 +0530

    removed the blogs code

--- app/page.js
+++ app/page.js
@@ -1,5 +1,4 @@
 "use client"
-import { personalData } from "@/utils/data/personal-data";
 import AboutSection from "./components/homepage/about";
 import ContactSection from "./components/homepage/contact";
 import Education from "./components/homepage/education";
@@ -9,23 +8,7 @@ import Projects from "./components/homepage/projects";
 import Skills from "./components/homepage/skills";
 import MouseTracker from "./components/homepage/mousetracker";
 
-async function getData() {
-  const res = await fetch(`https://dev.to/api/articles?username=${personalData.devUsername}`)
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data')
-  }
-
-  const data = await res.json();
-
-  const filtered = data.filter((item) => item?.cover_image).sort(() => Math.random() - 0.5);
-
-  return filtered;
-};
-
 export default async function Home() {
-  const blogs = await getData();
-
   return (
     <>
       <MouseTracker />
